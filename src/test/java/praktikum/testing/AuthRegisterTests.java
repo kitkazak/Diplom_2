ea9310e24c57ece1d84c1a3c00d838ef7e505586
Nikita Kazak <kitkazak@yandex.ru>
2023-06-12 11:37:33 +0300

    fixed auth register tests

--- src/test/java/praktikum/testing/AuthRegisterTests.java
+++ src/test/java/praktikum/testing/AuthRegisterTests.java
@@ -1,8 +1,11 @@
 package praktikum.testing;
 
+import io.qameta.allure.Description;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.Response;
 import org.json.simple.JSONObject;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.util.HashMap;
 import java.util.UUID;
@@ -13,53 +16,47 @@ import static org.hamcrest.Matchers.*;
 
 public class AuthRegisterTests {
 
-    @Test
-    @DisplayName("Создать уникального пользователя")
-    public void authRegisterUnique() {
-        UUID uuid = UUID.randomUUID();
+    UUID uuid;
+    String accessToken;
+    JSONObject registerBody;
+    Response registerResponse;
+
+    // В before проверяется создание уникального пользователя
+    // Поэтому тестов стало на один меньше
+    @Before
+    public void setUp() {
+        uuid = UUID.randomUUID();
 
         HashMap<String, Object> authRegisterBody = new HashMap<>();
         authRegisterBody.put("email", "kitkazak_email" + uuid.toString() + "@yandex.ru");
         authRegisterBody.put("password", "kitkazak_password" + uuid.toString());
         authRegisterBody.put("name", "kitkazak_name" + uuid.toString());
+        registerBody = new JSONObject(authRegisterBody);
 
-        Response res = Auth.register(new JSONObject(authRegisterBody));
-        res.then().statusCode(200);
+        registerResponse = Auth.register(registerBody);
+        accessToken = registerResponse.jsonPath().get("accessToken");
+        registerResponse.then().statusCode(200);
+    }
 
-        // Delete user
-        Response deleteRes = Auth.delete(res.jsonPath().get("accessToken"));
-        deleteRes.then().statusCode(202);
-
+    @After
+    public void tearDown() {
+        Response deleteResponse = Auth.delete(accessToken);
+        deleteResponse.then().statusCode(202);
     }
 
     @Test
     @DisplayName("Cоздать пользователя, который уже зарегистрирован")
     public void authRegisterUserAlreadyExists() {
-        UUID uuid = UUID.randomUUID();
-
-        HashMap<String, Object> authRegisterBody = new HashMap<>();
-        authRegisterBody.put("email", "kitkazak_email" + uuid.toString() + "@yandex.ru");
-        authRegisterBody.put("password", "kitkazak_password" + uuid.toString());
-        authRegisterBody.put("name", "kitkazak_name" + uuid.toString());
-
-        Response res = Auth.register(new JSONObject(authRegisterBody));
-        res.then().statusCode(200);
-
-        Response secondRes = Auth.register(new JSONObject(authRegisterBody));
+        Response secondRes = Auth.register(new JSONObject(registerBody));
         secondRes.then().statusCode(403).and().body("message", equalTo("User already exists"));
-
-        Response deleteRes = Auth.delete(res.jsonPath().get("accessToken"));
-        deleteRes.then().statusCode(202);
     }
 
     @Test
     @DisplayName("Создать пользователя и не заполнить одно из обязательных полей")
     public void authRegisterNotAllRequiredFields() {
-        UUID uuid = UUID.randomUUID();
-
         HashMap<String, Object> authRegisterBody = new HashMap<>();
-        authRegisterBody.put("email", "kitkazak_email" + uuid.toString() + "@yandex.ru");
-        authRegisterBody.put("name", "kitkazak_name" + uuid.toString());
+        authRegisterBody.put("email", "notallfields_email" + uuid.toString() + "@yandex.ru");
+        authRegisterBody.put("name", "notallfields_name" + uuid.toString());
 
         Response res = Auth.register(new JSONObject(authRegisterBody));
         res.then().statusCode(403)
