9dbc4073419427f6807e5f40f5dd23e533aa4a2b
Nikita Kazak <kitkazak@yandex.ru>
2023-06-12 11:31:24 +0300

    fixed auth login tests

--- src/test/java/praktikum/testing/AuthLoginTests.java
+++ src/test/java/praktikum/testing/AuthLoginTests.java
@@ -3,6 +3,8 @@ package praktikum.testing;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.Response;
 import org.json.simple.JSONObject;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.util.HashMap;
 import java.util.UUID;
@@ -12,46 +14,44 @@ import praktikum.testing.api.Auth;
 import static org.hamcrest.Matchers.*;
 
 public class AuthLoginTests {
-    @Test
-    @DisplayName("Логин под существующим пользователем")
-    public void authLoginExistingUser() {
-        UUID uuid = UUID.randomUUID();
+
+    UUID uuid;
+    String accessToken;
+    JSONObject registerBody;
+    Response registerResponse;
+
+    @Before
+    public void setUp() {
+        uuid = UUID.randomUUID();
 
         HashMap<String, Object> authRegisterBody = new HashMap<>();
         authRegisterBody.put("email", "kitkazak_email" + uuid.toString() + "@yandex.ru");
         authRegisterBody.put("password", "kitkazak_password" + uuid.toString());
         authRegisterBody.put("name", "kitkazak_name" + uuid.toString());
+        registerBody = new JSONObject(authRegisterBody);
 
-        Response res = Auth.register(new JSONObject(authRegisterBody));
-        res.then().statusCode(200);
+        registerResponse = Auth.register(registerBody);
+        accessToken = registerResponse.jsonPath().get("accessToken");
+        registerResponse.then().statusCode(200);
+    }
 
+    @After
+    public void tearDown() {
+        Response deleteResponse = Auth.delete(accessToken);
+        deleteResponse.then().statusCode(202);
+    }
+
+    @Test
+    @DisplayName("Логин под существующим пользователем")
+    public void authLoginExistingUser() {
         // Login user
-        HashMap<String, Object> loginBody = new HashMap<>();
-        loginBody.put("name", "kitkazak_name" + uuid.toString());
-        loginBody.put("password", "kitkazak_password" + uuid.toString());
-        loginBody.put("email", "kitkazak_email" + uuid.toString() + "@yandex.ru");
-        Response loginRes = Auth.login(new JSONObject(loginBody));
-        loginRes.then().statusCode(200);
-
-        // Delete user
-        Response deleteRes = Auth.delete(res.jsonPath().get("accessToken"));
-        deleteRes.then().statusCode(202);
-
+        Response loginResponse = Auth.login(registerBody);
+        loginResponse.then().statusCode(200);
     }
 
     @Test
     @DisplayName("Логин с неверным логином и паролем")
     public void authLoginInvalidCredentials() {
-        UUID uuid = UUID.randomUUID();
-
-        HashMap<String, Object> authRegisterBody = new HashMap<>();
-        authRegisterBody.put("email", "kitkazak_email" + uuid.toString() + "@yandex.ru");
-        authRegisterBody.put("password", "kitkazak_password" + uuid.toString());
-        authRegisterBody.put("name", "kitkazak_name" + uuid.toString());
-
-        Response res = Auth.register(new JSONObject(authRegisterBody));
-        res.then().statusCode(200);
-
         // Login user
         HashMap<String, Object> loginBody = new HashMap<>();
         loginBody.put("name", "kitkazak_name" + uuid.toString());
@@ -59,10 +59,5 @@ public class AuthLoginTests {
         loginBody.put("email", "kitkazak_email" + uuid.toString() + "@yandex.ru");
         Response loginRes = Auth.login(new JSONObject(loginBody));
         loginRes.then().statusCode(401).and().body("message", equalTo("email or password are incorrect"));
-
-        // Delete user
-        Response deleteRes = Auth.delete(res.jsonPath().get("accessToken"));
-        deleteRes.then().statusCode(202);
-
     }
 }
